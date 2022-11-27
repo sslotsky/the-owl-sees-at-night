d8541ea95d60b211fa8ffe22e8179a34d655ad73
Sam Slotsky <saxosamo@gmail.com>
2022-11-27 14:10:46 -0600

    Responsive header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,21 +1,34 @@
-import { component$, useStylesScoped$ } from '@builder.io/qwik';
+import { component$, useStylesScoped$, useSignal } from '@builder.io/qwik';
 import styles from './header.css?inline';
 
 export default component$(() => {
   useStylesScoped$(styles);
+  const open = useSignal(false);
 
   return (
     <header>
       <div class="logo">
         <h1>Bee Wednesday <strong>photography for humans</strong></h1>
       </div>
-      <ul>
+      <ul class="left-nav">
         <li>
           <a href="/photos">
             Latest Shots
           </a>
         </li>
       </ul>
+      <div class="top-nav">
+        <button onClick$={() => open.value = !open.value} class="menu">&#8801;</button>
+          {open.value && (
+            <ul>
+              <li>
+                <a href="/photos">
+                  Latest Shots
+                </a>
+              </li>
+            </ul>
+          )}
+      </div>
     </header>
   );
 });
