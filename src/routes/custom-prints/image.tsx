fbdb0e770a1409798bc3c194cbf7d11d095949bf
Sam Slotsky <saxosamo@gmail.com>
2023-01-22 19:44:49 -0600

    duplicate task

--- src/routes/custom-prints/image.tsx
+++ src/routes/custom-prints/image.tsx
@@ -1,4 +1,4 @@
-import { component$, Signal, useClientEffect$, useStore, useTask$ } from '@builder.io/qwik';
+import { component$, Signal, useClientEffect$, useStore } from '@builder.io/qwik';
 import { MasonryPhoto } from '~/trcp/router';
 import { useImageUrl } from './hooks';
 import { Variant } from './types';
@@ -30,14 +30,6 @@ export default component$((props: Props) => {
     initialized: false
   });
 
-
-  useTask$(async ({ track }) => {
-    track(() => props.store.gridView);
-
-    if (!props.store.gridView) {
-      props.store.zoomFactor = 1;
-    }
-  })
 
   useClientEffect$(async ({ track }) => {
     track(() => props.store.file);
