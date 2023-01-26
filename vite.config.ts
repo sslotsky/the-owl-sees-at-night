9315a51014723901a6ec14bf2f751f54b04e599a
Sam Slotsky <saxosamo@gmail.com>
2023-01-26 17:08:34 -0600

    Inline codegen config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,40 @@
-import { defineConfig } from 'vite';
-import { qwikVite } from '@builder.io/qwik/optimizer';
-import { qwikCity } from '@builder.io/qwik-city/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import codegen from 'vite-plugin-graphql-codegen';
+import { defineConfig, loadEnv } from "vite";
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import { qwikCity } from "@builder.io/qwik-city/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import codegen from "vite-plugin-graphql-codegen";
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths(), codegen()],
+    plugins: [
+      qwikCity(),
+      qwikVite(),
+      tsconfigPaths(),
+      codegen({
+        config: {
+          overwrite: true,
+          schema: `${process.env.VITE_VENDURE_URL}/shop-api`,
+          documents: "src/**/*.tsx",
+          generates: {
+            "src/gql/": {
+              preset: "client",
+              plugins: [],
+              config: {
+                enumsAsTypes: true,
+              },
+            },
+            "./graphql.schema.json": {
+              plugins: ["introspection"],
+            },
+          },
+        },
+      }),
+    ],
     preview: {
       headers: {
-        'Cache-Control': 'public, max-age=600',
+        "Cache-Control": "public, max-age=600",
       },
     },
   };
